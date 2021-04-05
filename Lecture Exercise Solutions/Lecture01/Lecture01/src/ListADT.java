164aff21a55a34dfb21a2b89af1d70d745c6426b
Rohan Patel <rohanspatel711711@gmail.com>
2021-04-05 14:39:44 -0600

    deleted unnecessary files.

--- Lecture Exercise Solutions/Lecture01/Lecture01/src/ListADT.java
+++
@@ -1,9 +0,0 @@
-// Exercise 8
-
-public interface ListADT<I> {
-    void insertFirst(I x);
-    void deleteFirst();
-    I firstItem();
-    boolean isEmpty();
-    boolean isFull();
-}
--- Lecture Exercise Solutions/Lecture10/Lecture10/src/ListADT.java
+++
@@ -1,8 +0,0 @@
-
-public interface ListADT<I> {
-    void insertFirst(I x);
-    void deleteFirst();
-    I firstItem();
-    boolean isEmpty();
-    boolean isFull();
-}
